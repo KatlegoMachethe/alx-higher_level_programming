7d44232be07e79fb7769b196ccc753e3c6cb97d8
KatlegoMachethe <kmi.machethe@gmail.com>
2022-07-20 00:32:22 +0200

    1st batch

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """Class defined by Private instance attribute"""
+
+    def __init__(self, size):
+        """This is private instance attribute
+
+        We initialize the class here
+        """
+
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+0x06. Python - Classes and Objects
+---
+In this directory we delve into Object Oriented Programming with Python.
