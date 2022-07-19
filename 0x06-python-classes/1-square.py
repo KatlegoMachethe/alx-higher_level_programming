07090213f4105fbb06f3816c817a94f646c8d852
KatlegoMachethe <kmi.machethe@gmail.com>
2022-07-20 00:35:21 +0200

    1st batch

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
 class Square:
     """Class defined by Private instance attribute"""
-
     def __init__(self, size):
-        """This is private instance attribute
-
-        We initialize the class here
-        """
-
+        """This is private instance attribute"""
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 class Square:
     """Class defines a square"""
-
     def __init__(self, size=0):
         """Initialize the class"""
-
         if type(size) is not int:
             raise TypeError("Size must be an integer")
         if size < 0:
