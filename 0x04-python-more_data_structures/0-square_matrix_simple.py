8cf637b63fcf0bc7e010d3b817d6eb447ab057c7
KatlegoMachethe <kmi.machethe@gmail.com>
2022-08-25 13:57:25 +0200

    Made files executable

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
+    """
+    Replaces all occurrences of an element
+    """
     if matrix:
-        return [list(map(lambda j: j**2, i)) for i in matrix]
+        return [list(map(lambda j: j**2, i)) for i in matrix]n
